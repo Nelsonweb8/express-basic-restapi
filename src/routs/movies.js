9f1faa46ebd09d2cfdd0adbd28ab0112a53909eb
NelsonrBermejo <bermejonelson.r@gmail.com>
2021-01-26 22:04:11 -0300

    obtengo propiedades desde body +  guardo en constantes cada uno de los valores que voy recibiendo

--- src/routs/movies.js
+++ src/routs/movies.js
@@ -9,9 +9,21 @@ router.get('/',(req, res)=>{
 });
 
 router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('received');
-})
+    const {title, director, year, rating} = req.body;
+    if (title && director && year && rating) {
+        const id = movies.length+1;
+        const newMovie = {...req.body, id};
+        //console.log(newMovie);
+        movies.push(newMovie);
+        //res.json('saved');
+        res.json(movies);//envio al cliente las peliculas acutalizadas
+    } else {
+        //res.send('wrong Request');
+        //res.json( {error: "There was an error."});
+        res.status(500).json({error: "There was an error."});
+    }
+});
+
 
 
 module.exports = router;
